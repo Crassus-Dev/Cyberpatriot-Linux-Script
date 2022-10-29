956c19c1527785fec271e231bc0160713270f62c
Crassus-Dev <95876194+Crassus-Dev@users.noreply.github.com>
2022-10-29 12:21:01 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,43 +1,45 @@
 #!/bin/bash
 
 function wait () {
-echo "should we continue? (y|n)"
-read -r user_input
-if [ $user_input == 'y' ]
-then
-	echo "continuing..."
-else
-	echo "exiting..."
-	exit 0
-fi
+	echo "should we continue? (y|n)"
+	read -r user_input
+	if [ $user_input == 'y' ]
+	then
+		echo "continuing..."
+	else
+		echo "exiting..."
+		exit 0
+	fi
 }
 function should () {
-echo "should I "$1" (y|n)"
-read -r user_input
-if [ $user_input == 'y' ]
-then
-	$1
-else
-	echo "skipping..."
-fi
+	echo "should I "$1" (y|n)"
+	read -r user_input
+	if [ $user_input == 'y' ]
+	then
+		$1
+	else
+		echo "skipping..."
+	fi
 }
 
 function update_and_upgrade () {
-apt-get update
-apt-get upgrade
+	apt-get update
+	apt-get upgrade
 }
 function edit_lightdm () {
-echo "allow-guest=false" >> /etc/lightdm/lightdm.conf
+	status = which lightdm	
+	echo "allow-guest=false" >> /etc/lightdm/lightdm.conf
 }
 function enable_ufw_firewall () {
-apt-get install ufw
-ufw enable
-ufw status
+	apt-get install ufw
+	ufw enable
+	ufw status
 }
 function enable_clamtk () {
-apt-get install clamtk
-freshclam
+	apt-get install clamtk
+	freshclam
 }
+
 
 should update_and_upgrade
 should edit_lightdm
