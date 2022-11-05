5d5ca4f5baca7a2720a7eac1df367bd572ae6ddc
tomaszzz <tomaszez@gmail.com>
2022-11-05 14:56:19 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -43,19 +43,19 @@ function edit_lightdm () {
 	# /usr/share/lightdm/lightdm.conf.d/*.conf
 	# /etc/lightdm/lightdm.conf.d/*.conf
 	# At least warn about these and handle manaully
-	dir=/tmp/etc/lightdm/lightdm.conf.d
+	dir=/etc/lightdm/lightdm.conf.d
 	check_dir "$dir" "Check lightdm configs in $dir"
 
-	dir=/tmp/usr/share/lightdm/lightdm.conf.d
+	dir=/usr/share/lightdm/lightdm.conf.d
 	check_dir "$dir" "Check lightdm configs in $dir"
 	
 	# 2) What if there is already an entry of 'allow-guest=true' or 'allow-guest=false' ?
-	file=./etc/lightdm/lightdm.conf
+	file=/etc/lightdm/lightdm.conf
 	allow_guest_exists=`grep "allow-guest" "$file"`
 	if [ ! -z "$allow_guest_exists" ]
 	then
 		# Update file replacing the matched allow-guest config parameter
-  		sed -i '' 's/allow-guest.*$/allow-guest=false/' "$file"
+  		sed -i 's/allow-guest.*$/allow-guest=false/' "$file"
   		echo "Updated $file"
 	else
 		echo "allow-guest=false" >> /etc/lightdm/lightdm.conf
