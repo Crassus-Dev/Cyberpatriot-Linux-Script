7385468ccfe0c03149637b76d71d532d12808af5
crassus-dev <kapinosek2@gmail.com>
2023-05-17 14:09:00 -0600

    Added ufw configurations and dist-upgrade

--- functions/enable_ufw_firewall.sh
+++ functions/enable_ufw_firewall.sh
@@ -1,5 +1,12 @@
 function enable_ufw_firewall () {
 	apt-get install ufw
+	ufw default deny incoming
+	ufw default allow outgoing
+	ufw allow ssh
+	ufw allow http
+	ufw allow https
+	ufw logging on
+	ufw logging high
 	ufw enable
 	ufw status
 }
